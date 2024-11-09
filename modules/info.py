f4771e23929d2925218a533a10bc5239606d4958
TheKliko <121750609+TheKliko@users.noreply.github.com>
2024-11-09 10:14:59 +0100

    Add files via upload

--- modules/info.py
+++ modules/info.py
@@ -1,6 +1,6 @@
 class ProjectData:
     NAME: str = "Kliko's modding tool"
-    VERSION: str = "1.3.1"
+    VERSION: str = "1.3.2"
     DESCRIPTION: str = "Roblox mods made easy"
     AUTHOR: str = "TheKliko"
 
