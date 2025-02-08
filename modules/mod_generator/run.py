e9a7d301d1ee1445cb54999b7ad9c8ca69c06ac1
TheKliko <121750609+TheKliko@users.noreply.github.com>
2025-02-08 10:07:32 +0100

    Generate mods for latest Player version
    instead of generating for the latest Studio version

--- modules/mod_generator/run.py
+++ modules/mod_generator/run.py
@@ -5,8 +5,10 @@ from pathlib import Path
 from tempfile import TemporaryDirectory
 
 from modules import Logger
+from modules.functions.roblox import latest_version
 from modules.launcher.deployment_info import Deployment
 
+from .deploy_history import DeployHistory, DeployHistoryError, get_deploy_history
 from .download_luapackages import download_luapackages
 from .locate_imagesets import locate_imagesets
 from .locate_imagesetdata_file import locate_imagesetdata_file
@@ -25,7 +27,18 @@ def run(name: str, color1, color2, angle: int, output_dir: str | Path, user_sele
     if output_target.exists():
         raise FileExistsError("Cannot generate a mod that already exists!")
 
-    deployment: Deployment = Deployment("Studio")
+    # Generate mods for latest Player version, instead of the latest Studio version
+    try:
+        version: str = latest_version.get("WindowsPlayer")
+        deploy_history: DeployHistory = get_deploy_history(version)
+        player_equivalent_version: str = deploy_history.LatestVersion.player
+        studio_version: str = deploy_history.LatestVersion.studio
+
+    # If not possible, generate for the latest Studio version instead
+    except DeployHistoryError:
+        deployment = Deployment("Studio")
+        player_equivalent_version = deployment.version
+        studio_version = deployment.version
 
     with TemporaryDirectory(prefix=f"mod_generator_") as tmp:
         temporary_directory: Path = Path(tmp)
@@ -33,22 +46,22 @@ def run(name: str, color1, color2, angle: int, output_dir: str | Path, user_sele
         temp_target.mkdir(parents=True, exist_ok=True)
 
         Logger.info("Writing info.json...")
-        data: dict = {"clientVersionUpload": deployment.version, "watermark": "Generated with Kliko's mod generator"}
+        data: dict = {"clientVersionUpload": player_equivalent_version, "watermark": "Generated with Kliko's mod generator"}
         with open(temp_target / "info.json", "w") as file:
             json.dump(data, file, indent=4)
 
         Logger.info("Downloading LuaPackages...")
-        download_luapackages(deployment.version, temporary_directory)
+        download_luapackages(studio_version, temporary_directory)
 
         Logger.info("Locating ImageSets...")
-        imageset_path: Path = locate_imagesets(temporary_directory / deployment.version)
-        shutil.copytree((temporary_directory / deployment.version / imageset_path), (temp_target / imageset_path), dirs_exist_ok=True)
+        imageset_path: Path = locate_imagesets(temporary_directory / studio_version)
+        shutil.copytree((temporary_directory / studio_version / imageset_path), (temp_target / imageset_path), dirs_exist_ok=True)
         
         Logger.info("Locating ImageSetData...")
-        imagesetdata_path: Path = locate_imagesetdata_file(temporary_directory / deployment.version)
+        imagesetdata_path: Path = locate_imagesetdata_file(temporary_directory / studio_version)
         
         Logger.info("Getting icon map...")
-        icon_map: dict[str, dict[str, dict[str, str | int]]] = get_icon_map(temporary_directory / deployment.version / imagesetdata_path)
+        icon_map: dict[str, dict[str, dict[str, str | int]]] = get_icon_map(temporary_directory / studio_version / imagesetdata_path)
 
         Logger.info("Generating modded ImageSets...")
         generate_imagesets((temp_target / imageset_path), icon_map, color1, color2, angle)
