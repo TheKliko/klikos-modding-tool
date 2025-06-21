f5f75ec8b0bf222fd201c4120e1b24a20ffc276f
TheKliko <121750609+TheKliko@users.noreply.github.com>
2025-06-21 14:17:04 +0200

    Update version, licenses, contributors

--- Kliko's modding tool/modules/project_data/info.py
+++ Kliko's modding tool/modules/project_data/info.py
@@ -1,5 +1,5 @@
 NAME: str = "Kliko's modding tool"
-VERSION: str = "2.0.2"
+VERSION: str = "2.0.3"
 DISCORD: str = r"https://discord.gg/nEjUwdSP9P"
 REPOSITORY: str = r"https://github.com/thekliko/klikos-modding-tool"
 WIKI: str = r"https://github.com/thekliko/klikos-modding-tool/wiki"
